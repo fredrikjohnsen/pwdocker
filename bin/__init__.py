0850d87e18071abae05bf96457dc816da1c86586
Sekkingstad <andreas.sekkingstad@bergen.kommune.no>
2022-06-22 11:41:19 +0200

    UT-3577 legger til imports i __init__.py fil

---
+++ bin/__init__.py
@@ -0,0 +1,7 @@
+from .html2pdf import html2pdf
+from .mhtml2pdf import mhtml2pdf
+from .office2pdf import office2pdf
+from .pdf2pdfa import pdf2pdfa
+from .pdf2text import pdf2text
+from .text2utf8 import text2utf8
+from unoconv2x import unoconv2x
