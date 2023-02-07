9e58e63a452eb72e8a59e77768e370a49d8eeac9
Frode Kirkholt <xs435@lxbyarkiv01i.srv.bergenkom.no>
2023-02-07 14:52:46 +0100

    Fix pdf/a conversion for files with space in filename

--- bin/pdf2pdfa.sh
+++ bin/pdf2pdfa.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gs -q -dPDFA=2 -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -dPDFACompatibilityPolicy=1 -sOutputFile=$2 $1
+gs -q -dPDFA=2 -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -dPDFACompatibilityPolicy=1 -sOutputFile="$2" "$1"
