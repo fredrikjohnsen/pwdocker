a83e87798d7e347a0dda40bf003c20286cdd3844
fkirkholt <fkirkholt@yahoo.no>
2024-01-03 12:29:15 +0100

    Quote file names when extracting zip files

--- bin/unzip.py
+++ bin/unzip.py
@@ -1,21 +1,22 @@
 import subprocess
+from shlex import quote
 import typer
 
 
 def unzip(zipfile, to_dir):
+    """ Unzip file with correct encoding for norwegian """
     encoding = None
     for enc in ['IBM850', 'windows-1252']:
-        print(enc)
-        cmd = [f'lsar -e {enc} {zipfile}']
+        cmd = [f"lsar -e {enc} {quote(zipfile)}"]
         out = run_process(cmd)
         if 'æ' in out or 'ø' in out or 'å' in out:
             encoding = enc
             break
 
     if encoding:
-        cmd = [f'unar -k skip -D -e {encoding} {zipfile} -o {to_dir}']
+        cmd = [f"unar -k skip -D -e {encoding} {quote(zipfile)} -o {quote(to_dir)}"]
     else:
-        cmd = [f'unar -k skip -D {zipfile} -o {to_dir}']
+        cmd = [f"unar -k skip -D {quote(zipfile)} -o {quote(to_dir)}"]
 
     run_process(cmd)
 
