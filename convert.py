fredrikjohnsen/pwdocker

commit 7e43418daefbedf8082dc18570766e06915645a0

Don't report accepted files during conversion

convert.py
@@ -265,7 +265,7 @@ def convert_folder(
                                     debug, set_source_ext, identify_only)
 
             # If conversion failed
-            if norm is False:
+            if norm is False and src_file.status != 'accepted':
                 console.print('  ' + src_file.status, style="bold red")
             elif type(norm) is str:
                 dest_path = Path(dest_dir, norm)

commit 01c4e2521ae217297899b227b5930e39431ee386

Fix error with accepted files
Regression from 7e43418

convert.py
@@ -271,8 +271,9 @@ def convert_folder(
                                     debug, set_source_ext, identify_only)
 
             # If conversion failed
-            if norm is False and src_file.status != 'accepted':
-                console.print('  ' + src_file.status, style="bold red")
+            if norm is False:
+                if src_file.status != 'accepted':
+                    console.print('  ' + src_file.status, style="bold red")
             elif type(norm) is str:
                 dest_path = Path(dest_dir, norm)
                 unpacked_count = sum([len(files) for r, d, files